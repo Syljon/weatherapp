0bc4c2ceeb10648f5d804943742fab104b4b1322
Lukasz <syljon2@gmail.com>
2019-05-19 21:05:47 +0200

    Updates

---
+++ precache-manifest.3f583d980a340bd9ebd67ad296139465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca6962b4c96d279a259cd68bbdbee27",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "707235a2687655e1fc98",
+    "url": "/weatherapp/static/css/main.38096cad.chunk.css"
+  },
+  {
+    "revision": "b31c88c9b0bc60e21d4e",
+    "url": "/weatherapp/static/js/2.93f110b2.chunk.js"
+  },
+  {
+    "revision": "707235a2687655e1fc98",
+    "url": "/weatherapp/static/js/main.61723ddb.chunk.js"
+  },
+  {
+    "revision": "5bfbd7a83723d1d6555c",
+    "url": "/weatherapp/static/js/runtime~main.e7dc3139.js"
+  },
+  {
+    "revision": "ba2f6a8e8fc9007c938d4bda65bac3da",
+    "url": "/weatherapp/static/media/cloud.ba2f6a8e.png"
+  }
+]);
